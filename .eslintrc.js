acf0039e507c0e6cd4c81018ce6cc15ed03946f3
cedricahenkorah <cedricahenkorah@gmail.com>
2025-07-12 15:07:31 +0000

    feat: initial project setup with TypeScript, ESLint, and Prettier configuration

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: ["eslint:recommended", "@typescript-eslint/recommended"],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  rules: {
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "prefer-const": "error",
+    "no-var": "error",
+  },
+};
